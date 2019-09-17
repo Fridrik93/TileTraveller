47a50700cd5be705c47e7d8a14d5156cf02eff77
Fridrik93 <fridrik19@ru.is>
2019-09-17 11:01:27 +0000

    Fixed x,y cords

--- tile_traveller.py
+++ tile_traveller.py
@@ -3,10 +3,10 @@
 # hafa max up sem 1 og max down sem 3 ef náð er 1 eða 3 verður talan 1 eða 3 
 
 #check ef x skipun er innan marka
-position_x =0
+
 def limit_check_x(input_x, input_y):
-    max_left = 5
-    max_right = 7
+    max_left = 1
+    max_right = 3
     
     if input_x < max_left:
         input_x = max_left
@@ -20,11 +20,12 @@ def limit_check_x(input_x, input_y):
         input_y = max_up
     return input_x, input_y
 
-def wall_check(input_x, input_y):
-    firstwall_x= 5
-    secondwall_x = 6
-    thirdwall_x = 7
-    firstwall_y = 2
+def wall_check(input_x, input_y, direction):
+    # firstwall_x= 5
+    # secondwall_x = 6
+    # thirdwall_x = 7
+    # firstwall_y = 2
+
 
     
     
@@ -34,7 +35,7 @@ def limit_check_y(input):
    
 # check if in victory position
 def victory_check(input_x, input_y):
-    max_x= 7
+    max_x= 3
     max_y= 3
     if input_x == max_x and input_y == max_y:
         print('Voctory!')       
@@ -54,7 +55,7 @@ def direction(pos_x, pos_y, direction):
     return pos_x, pos_y
 
 #op= original position
-op_x = 5
+op_x = 1
 op_y = 1
 
 direction= input("You can travel: ")
