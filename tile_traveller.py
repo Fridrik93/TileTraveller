5069f9af8b6121471e36811e35bc29977a16eae1
ignas96 <ignas18@ru.is>
2019-09-17 09:53:42 +0000

    Description

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,3 @@
-##
+# línur eru skilbreindar sem 1, 2, 3 og dálkar 5, 6, 7 
+# myndum hafa max left sem 5 og max right sem 7 ef náð er 7 eða 5 verður talan bara 5 eða 7
+# hafa max up sem 1 og max down sem 3 ef náð er 1 eða 3 verður talan 1 eða 3 
